c7c2159e03b1f1e349408671f3852427bafe3098
MarjorieDaenecke <marjoriedaenecke@gmail.com>
2025-06-29 17:34:41 -0300

    tela_cadastro_atualizada

--- app/cadastro.tsx
+++ app/cadastro.tsx
@@ -1,69 +1,17 @@
-import { navigate } from 'expo-router/build/global-state/routing';
 import React, { useState } from 'react';
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
-  StyleSheet,
   Alert,
   Image,
   SafeAreaView,
 } from 'react-native';
+import { navigate } from 'expo-router/build/global-state/routing';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFA726', // laranja de fundo (pode usar gradiente também)
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  logo: {
-    width: 150,
-    height: 100,
-    resizeMode: 'contain',
-    marginBottom: 20,
-  },
-  form: {
-    backgroundColor: '#FFE0B2',
-    borderRadius: 20,
-    padding: 20,
-    width: '100%',
-    maxWidth: 300,
-    marginBottom: 20,
-  },
-  label: {
-    color: '#D32F2F',
-    fontWeight: 'bold',
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  input: {
-    backgroundColor: '#FFF3E0',
-    padding: 10,
-    borderRadius: 20,
-    marginBottom: 15,
-  },
-  button: {
-    backgroundColor: '#CCFF00',
-    paddingVertical: 12,
-    paddingHorizontal: 40,
-    borderRadius: 20,
-    marginBottom: 20,
-  },
-  buttonText: {
-    color: '#D32F2F',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  footerText: {
-    color: '#D32F2F',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-  },
-});
-
+// Importando o estilo separado
+import styles from './styles/registerStyles';
 
 const RegisterScreen: React.FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -80,8 +28,8 @@ const RegisterScreen: React.FC = () => {
       Alert.alert('Erro de Cadastro', 'Por favor, insira um email válido (ex: seu.email@exemplo.com).');
       return;
     }
-    if (nome.length <= 2 ){
-      Alert.alert('Erro de Cadastro', 'Insira um nome válido, ao menos 3 letras');
+    if (nome.length <= 2) {
+      Alert.alert('Erro de Cadastro', 'Insira um nome válido, ao menos 3 letras.');
       return;
     }
     if (!passwordRegex.test(senha)) {
@@ -92,7 +40,7 @@ const RegisterScreen: React.FC = () => {
       Alert.alert('Erro de Cadastro', 'As senhas não coincidem!');
       return;
     }
-      
+
     try {
       const response = await fetch(`${API_URL}/cadastro`, {
         method: 'POST',
@@ -117,7 +65,7 @@ const RegisterScreen: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Image
-        source={require('../assets/images/favicon.png')} // atualize o caminho da imagem
+        source={require('../assets/images/logo.png')}
         style={styles.logo}
       />
 
